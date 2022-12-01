44f897ea57e40b00dad80c96ba187e0444869576
SAIKAT SAMANTA <54322198+Saikatsamanta-glitch@users.noreply.github.com>
2022-12-01 11:27:27 +0530

    Add files via upload

---
+++ weatherjs.js
@@ -0,0 +1,24 @@
+// https://api.openweathermap.org/data/2.5/weather?q=bangalore&appid=482ab80833128b8817b5eb487df1942a
+console.log("start 🌱");
+
+
+
+const fetchData = async(city) => {
+    const response=await axios(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=482ab80833128b8817b5eb487df1942a`);
+    
+    console.log((response.data.main.temp-273.15).toFixed(2));
+
+    document.getElementsByClassName('temperature')[0].innerText=(response.data.main.temp-273.15).toFixed(2);
+    
+    console.log("middle 🎉")
+    document.getElementsByClassName('city')[0].value="";
+
+}
+function search(){
+    var city = document.getElementsByClassName('city')[0].value;
+    fetchData(city)
+    
+}
+
+
+console.log("end ⭐");
